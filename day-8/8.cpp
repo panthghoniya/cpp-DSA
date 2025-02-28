4e1d024b36832c1707f6d201f195929a245f88e2
panthghoniya <ghoniyapanth5@gmail.com>
2025-02-28 16:00:16 +0530

    Add files via upload

---
+++ day-8/8.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int box=1;
+
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = i; j >= 1; j--)
+        {
+            cout << box << " ";
+            box++;
+        }
+        cout << endl;
+    }
+    
+}
